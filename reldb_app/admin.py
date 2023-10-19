894195648f880d9cb6f1c9f4b2d98d5d93a2f4b8
Yoichiro Miki <y.miki.485@gmail.com>
2023-10-19 20:55:19 +0900

    created new branch dev1019

--- reldb_app/admin.py
+++ reldb_app/admin.py
@@ -4,17 +4,20 @@ from .models import Enrollment, Student, Instructor, Course, Course_student
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'enrollment')
+    list_filter = ('enrollment', 'current_status')
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title_short', 'title', 'instructor1')
+    list_filter = ('acad_year', 'quarter')
 
 class CourseStudentAdmin(admin.ModelAdmin):
     list_display = ('student', 'course')
 
 
+
 admin.site.register(Enrollment)
-#admin.site.register(Student, StudentAdmin)
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
+#admin.site.register(Student)
 admin.site.register(Instructor)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Course_student, CourseStudentAdmin)
